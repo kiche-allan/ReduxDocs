5ae0a4176400a9601343b09f5bf6004053583f85
Allan Kiche <tle2819@students.mu.ac.ke>
2023-05-22 10:51:28 +0300

    tasks

---
+++ src/components/Task.js
@@ -0,0 +1,4 @@
+// Task and TaskList will be stateless functional components, introduced in React
+// v0.14. They don’t have access to lifecycle methods such as componentDidMount, only
+// accept props, don’t use this.state or this.setState, and they’re defined as plain
+// functions instead of with createReactClass or ES2015 classes
